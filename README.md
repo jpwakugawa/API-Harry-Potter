03c384dd0ce25b3816c99441ee5fa0721a69fbbe
jpwakugawa <jokers2s2@outlook.com>
2021-11-21 08:26:48 -0400

    NEW: Loop infinito & Interatividade

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# API-Harry-Potter
-Projeto realizado para praticar os conceitos de API.
+## Avaliação Sprint 1 - Programa de Bolsas Compass.uol e UFMS
+- Primeira sprint do programa de bolsas Compass.uol para formação em chatbot Rasa.
+
+## Acesso ao app
+- https://harrypotter-jpwakugawa.herokuapp.com/
+
+## Harry Potter
+- Criador: João Paulo de Souza Wakugawa.
+- Projeto: Desenvolver um site que mostre diversos personagens de Harry Potter em um loop infinito permitindo que o usuário escolha a sua casa favorita.
+- Entre no app e veja a magia acontecer.
+
+## Informações
+- API utilizada: http://hp-api.herokuapp.com/api/characters
+- Hospedagem: Heroku & Github
+- Front-end: Html & CSS
+- Back-end: NodeJS  
+
+## Acesso local
+### Localhost: 3000
+```
+$ git clone
+$ npm install (Deve ser no diretório especificado)
+```
+
+## Upando no Heroku
+```
+$ heroku login
+$ heroku container:login
+$ heroku create  (Anote o nome do site criado)
+$ docker build -t registry.heroku.com/'nome-do-app-criado'/web . (Não esqueça de especificar o diretório)
+$ docker push registry.heroku.com/'nome-do-app-criado'/web
+$ heroku container:release web -a 'nome-do-app-criado'
+$ heroku open -a 'nome-do-app-criado'
+```
+
+## Buildando a imagem
+```
+$ docker build -t 'nome-da-img' . 
+$ docker run -it -p 'porta':3000 'nome-da-img'
+```
--- script.js
+++ script.js
@@ -1,31 +1,103 @@
-function showCharacter(character)
-{
+// Main function
+async function start() {
+	let casa = sessionStorage.getItem("house")
+	if(casa == null) {
+		casa = "gryffindor"
+	}
+
+	else if(casa == "slytherin") {
+		slythTheme()
+	}
+
+	else if(casa == "ravenclaw") {
+		ravenTheme()
+	}
+
+	else if(casa == "hufflepuff") {
+		huffTheme()
+	}
+
+	const lista = await getLista(casa)
+
+	while(true) {
+		let random = getRandomInt(0, 10)
+		await new Promise(resolve => setTimeout(() => resolve(showCharacter(lista, random)), 3000))
+	}
+}
+
+// Minor functions
+async function getLista(casa) {
+	try{	
+		const response = await fetch(`https://hp-api.herokuapp.com/api/characters/house/${casa}`)
+		const lista = await response.json()
+		return lista
+	}
+	catch(error){	
+		throw new Error(error.message) 
+	}
+}
+
+function showCharacter(lista, index) {
+	let character = lista[index]
 	let image = document.querySelector("[data-img]")
 	let name = document.querySelector("[data-name]")
 	let casa = document.querySelector("[data-casa]")
+	let dateOfBirth = document.querySelector("[data-birth]")
 	image.src = character.image
 	name.innerHTML = character.name
 	casa.innerHTML = character.house
+	dateOfBirth.innerHTML = character.dateOfBirth
 }
 
-async function getCharacter(cont)
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function setHouse(house) {
+	sessionStorage.setItem("house", house)
+	document.location.reload()
+}
+
+// Theme functions
+function gryffTheme()
 {
-	try{	
-		const response = await fetch("https://hp-api.herokuapp.com/api/characters")
-		const lista = await response.json()
-		let character = lista[cont]
-		showCharacter(character)
-	}
-	catch(error){	
-		console.log("Putz...")
-	}
+	let mainBox = document.querySelector(".main")
+	let optionsBox = document.querySelector(".options-box")
+	
+	mainBox.style.background= "#AE0001"
+	optionsBox.style.background = "#EEBA30"
 }
 
-async function start()
+function slythTheme()
 {
-	getCharacter(0)
-	for(let i=1; i<15; i++)
-		await new Promise(resolve => setTimeout(() => resolve(getCharacter(i)), 3000))
+	let mainBox = document.querySelector(".main")
+	let optionsBox = document.querySelector(".options-box")
+	
+	mainBox.style.background= "#2A623D"
+	optionsBox.style.background = "#AAAAAA"
 }
 
-start()
+function ravenTheme()
+{
+	let mainBox = document.querySelector(".main")
+	let optionsBox = document.querySelector(".options-box")
+	
+	mainBox.style.background= "#222F5B"
+	optionsBox.style.background = "#5D5D5D"
+}
+
+function huffTheme()
+{
+	let mainBox = document.querySelector(".main")
+	let optionsBox = document.querySelector(".options-box")
+	let textBox = document.querySelector(".text-box")
+	
+	mainBox.style.background= "#FFED86"
+	optionsBox.style.background = "#60605C"
+	textBox.style.color = "black"
+}
+
+// Start
+window.onload = start()
